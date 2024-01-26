79b048936b1d5f631c93d36f3f886383ea378975
di4ko <di4ko.dima@gmail.com>
2024-01-26 19:28:16 +0300

    Работа спавнера по таймеру

--- Assets/Scrips/Enemys/Enemy_Spawner.cs
+++ Assets/Scrips/Enemys/Enemy_Spawner.cs
@@ -4,38 +4,46 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEngine.EventSystems.EventTrigger;
 
 public class Enemy_Spawner : MonoBehaviour
 {
+    public float delay = 1;
+    float timer = 0;
+    List<(string, float)> CurrentWave;
 
-    public void spawnWave((string, float)[] EnemysWave) {
-        float Delay = 0;
-        foreach ((string Enemy, float delay) in EnemysWave)
-        {
-            Delay += delay * 1000;
-            spawnEnemy(Enemy, Delay);
-        }
-    }
-
-
-    private void spawnEnemy(string Enemy, float delay) {
-        GameObject prefabObject = Resources.Load<GameObject>($"Prefabs/Enemies/{Enemy}");
-        prefabObject.GetComponent<Enemy_Goblin>().WayPoint = transform.parent.transform.parent.transform.GetChild(1).transform.GetChild(0).GetComponent<WayPointRandom>();
-
-        GameObject instantiatedObject = Instantiate(prefabObject, transform.position, new Quaternion());
+    public void spawnWave(List<(string, float)> EnemysWave) {
+        CurrentWave = EnemysWave;
     }
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > delay)
+        {
+            Debug.Log("SDSD");
+            timer = 0;
+            if (CurrentWave.Count > 0)
+            {
+                string part = CurrentWave[0].Item1;
+
+                GameObject Enemy = Resources.Load<GameObject>($"Prefabs/Enemies/{part}");
+
+                Debug.Log(Enemy.ToString() + "объект");
+
+                Enemy.GetComponent<Enemy_Goblin>().WayPoint = transform.parent.transform.parent.transform.GetChild(1).transform.GetChild(0).GetComponent<WayPointRandom>();
+
+                Debug.Log("OOOOOOOOOOOOO");
+                GameObject instantiatedObject = Instantiate(Enemy, transform.position, new Quaternion());
+            }
+        }
     }
 }
