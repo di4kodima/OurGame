d16f2ea80071a9def3638ee1097b03d7aeead9d3
At1set <iliya9874329@gmail.com>
2024-01-26 15:12:46 +0300

    Теперь гоблин поварачивается в ту сторону, куда идет (Добавлен метод ChangeMovingSprite).

--- Assets/Scrips/Enemys/GoblinScipt.cs
+++ Assets/Scrips/Enemys/GoblinScipt.cs
@@ -6,14 +6,17 @@ using UnityEngine.EventSystems;
 
 public class Enemy_Goblin : MonoBehaviour
 {
-    public float Speed = 0.1f;
+    public float Speed = 0.25f;
+    public Sprite Goblin_Front;
+    public Sprite Goblin_Back;
+    public Sprite Goblin_Right;
+    public Sprite Goblin_Left;
     public WayPointRandom WayPoint;
-
     private Vector3 MoveDirection;
 
     void Start()
     {
-        
+    
     }
     public void Move()
     {
@@ -21,9 +24,23 @@ public class Enemy_Goblin : MonoBehaviour
 
         MoveDirection = Vector3.Normalize(WayPoint.transform.position - transform.position);
         transform.position += MoveDirection * Time.deltaTime * Speed;
+        Debug.Log(MoveDirection);
         
         if (Vector3.Distance(transform.position, WayPoint.transform.position) < 0.1f) ChangePoint();
+
+        ChangeMovingSprite();
     }
+
+    void ChangeMovingSprite() {
+        SpriteRenderer spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        float maxCoord = math.max(math.abs(MoveDirection.x), math.abs(MoveDirection.y));
+
+        if (maxCoord == MoveDirection.x) spriteRenderer.sprite = Goblin_Right;
+        else if (maxCoord == -MoveDirection.x) spriteRenderer.sprite = Goblin_Left;
+        if (maxCoord == MoveDirection.y) spriteRenderer.sprite = Goblin_Back;
+        else if (maxCoord == -MoveDirection.y) spriteRenderer.sprite = Goblin_Front;
+    }
+
     public void ChangePoint()
     {
         WayPoint =  (WayPointRandom)WayPoint.GetNextPoint();
