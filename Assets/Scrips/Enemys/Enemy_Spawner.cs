166a143579e3ae0200beb475c1ffbf96b67f5ee7
At1set <iliya9874329@gmail.com>
2024-01-29 00:52:29 +0300

    1) Теперь у врагов проигрывается анимация в зависимости от направления. 2) Когда враги доходят до последнего WayPoint-а, они исчезают. (Надо бы туда какой то метод засунуть, типо EnemyComingToEnd) 3) Немного почистил код и приукрасил комментариями [только в Goblin_script :(].

--- Assets/Scrips/Enemys/Enemy_Spawner.cs
+++ Assets/Scrips/Enemys/Enemy_Spawner.cs
@@ -1,30 +1,29 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Timers;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEngine.EventSystems.EventTrigger;
 
 public class Enemy_Spawner : MonoBehaviour
 {
-    public float delay = 1;
+    public float delay = 5;
     float timer = 0;
     List<(string, float)> CurrentWave;
+
+
 
     public void spawnWave(List<(string, float)> EnemysWave) {
         CurrentWave = EnemysWave;
         delay = CurrentWave[0].Item2;
     }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
+
+
     void Update()
     {
         timer += Time.deltaTime;
@@ -34,9 +33,11 @@ public class Enemy_Spawner : MonoBehaviour
             if (CurrentWave.Count > 0)
             {
                 string part = CurrentWave[0].Item1;
-
+        
                 GameObject Enemy = Resources.Load<GameObject>($"Prefabs/Enemies/{part}");
+                Debug.Log(Enemy);
                 Enemy.GetComponent<Enemy_Goblin>().WayPoint = transform.parent.transform.parent.transform.GetChild(1).transform.GetChild(0).GetComponent<WayPointRandom>();
+                Debug.Log(Enemy.GetComponent<Enemy_Goblin>().WayPoint);
                 GameObject instantiatedObject = Instantiate(Enemy, transform.position, new Quaternion());
             }
         }
--- Assets/Scrips/Enemys/Wave_Controller.cs
+++ Assets/Scrips/Enemys/Wave_Controller.cs
@@ -1,19 +1,28 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
 
 public class Wave_Controller : MonoBehaviour
 {
-    public List<(string, float)> wave_1 = new List<(string, float)>{ ("Goblin", 10), ("Goblin", 10), ("Goblin", 10), ("Goblin", 10), ("Goblin", 10), ("Goblin", 10), ("Goblin", 10), };
-    // Start is called before the first frame update
+    public List<(string, float)> wave_1 = new List<(string, float)>{ ("Goblin", 1), ("Goblin", 1), ("Goblin", 1), ("Goblin", 1), };
+    private Enemy_Spawner enemy_Spawner;
+
+
+
+    private void Awake()
+    {
+        transform.GetChild(0).GetComponent<Enemy_Spawner>();
+    }
+
+
+
     void Start()
     {
         Enemy_Spawner enemy_Spawner = transform.GetChild(0).GetComponent<Enemy_Spawner>();
         enemy_Spawner.spawnWave(wave_1);
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
         
